8a3685e8ba425cce20f49b61047882dac40d0002
James Campbell <jscampbell43@gmail.com>
2021-12-04 17:15:03 -0800

    Adding API setup, currently using test API

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import requests, json
+from pprint import pprint
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+
+my_key = 'D8FJrAVDcE5RHJ29uwD5lRftLXMDO6Tw3iGnj19V'
+
+payload = {
+  'api_key': my_key,
+  'start_date': '2017-03-09',
+  'end_date': '2017-03-11'
+}
+endpoint = 'https://jsonplaceholder.typicode.com/posts'
+try:
+  r = requests.get(endpoint, params=payload)
+  data = r.json()
+  pprint(data[0:10])
+except:
+  print('please try again')
+
 
 # Ignore this just a place holder for whatever we are passing
 my_info = {
@@ -13,4 +32,4 @@ my_info = {
 
 @app.route('/')
 def home():
-  return render_template('index.html', my_data = my_info)
+  return render_template('index.html', my_data = data)
