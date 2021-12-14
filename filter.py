945184de20c183db95a4b8d581406af3b54bdaec
aretinoco <artinoco@csumb.edu>
2021-12-13 16:44:46 -0800

    filter 2

--- filter.py
+++ filter.py
@@ -1,19 +1,28 @@
-input = ""
+# input = "p"
 
- #include in search
- def include(input):
-     #match with the search results or something?
-     results = "penauts"
-     return True if input in results else False
+#  #include in search
+#  def include(input):
+#      #match with the search results or something?
+#      results = "penauts"
+#      return True if input in results else False
 
-filtered = filter(include, input)
+# filtered = filter(include, input)
 
-#print the results??
+# #print the results??
 
-#exlude side?
- def exclude(input):
-     #match with the search results or something?
-     results = "penauts"
-     return False if input in results else True
+# #exlude side?
+#  def exclude(input):
+#      #match with the search results or something?
+#      results = "penauts"
+#      return False if input in results else True
 
-filtered = filter(include, input)
+# filtered = filter(include, input)
+
+#NEW PLAN
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('filter.html')
